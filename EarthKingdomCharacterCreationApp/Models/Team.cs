1930ac46daddff8faefe01dde67cac633f847e51
Andrew Lingo <andrew.j.lingo@gmail.com>
2022-01-20 10:42:32 -0500

    Created Team Controller and CRUD

---
+++ EarthKingdomCharacterCreationApp/Models/Team.cs
@@ -0,0 +1,27 @@
+﻿using EarthKingdomCharacterCreationApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EarthKingdomCharacterCreationApp.Models
+{
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        public string TeamName { get; set; }    
+        public List<Character> TeamMembers { get; set;} = new List<Character>();
+        public Team() { }
+        public Team (string teamName, List<Character> characterList)
+        {
+            TeamName = teamName;
+            TeamMembers = new List<Character>();
+            TeamMembers = characterList;
+        }
+
+
+    }
+}
